d057edc0eeb4ee9725b5d53a67918d489ec01ad5
MinoLou <elegirlourdes@gmail.com>
2019-05-26 23:20:47 -0300

    armé repo del proyecto

---
+++ proyecto.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Proyecto</title>
+    	<link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+<header class="container">
+<nav>
+  <ul>
+    <li><a id="logo" href="nosotros">Construworld</a></li>
+    <li><a class="nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
+    <li><a class="nav1" href="servicios.php">Servicios</a></li>
+    <li><a class="nav1" href="registro.php">Registration</a></li>
+    <li><a class="nav1" href="login.php">Login</a></li>
+  </ul>
+</nav>
+</header>
+<h2></h2>
+<section class="container">
+<article class="">
+
+</article>
+<article class="">
+
+</article>
+<article class="">
+
+</article>
+<article class="">
+
+</article>
+</section>
+<footer>
+  <a class="foot" href="contacto.php">Contacto</a>
+</footer>
+  </body>
+</html>
