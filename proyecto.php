77991a64b2bf6e7feef1ddd7a800dc9ea1923344
juanmartinibarguren@gmail.com <juanmartinibarguren@gmail.com>
2019-05-28 21:54:46 -0300

    agregue info a los cuadros de la pagina principal

--- proyecto.php
+++ proyecto.php
@@ -12,7 +12,7 @@
     <li><a id="logo" href="nosotros">Construworld</a></li>
     <li><a class="nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
     <li><a class="nav1" href="servicios.php">Servicios</a></li>
-    <li><a class="nav1" href="registro.php">Registration</a></li>
+    <li><a class="nav1" href="registro.php">Registracion</a></li>
     <li><a class="nav1" href="login.php">Login</a></li>
   </ul>
 </nav>
@@ -20,15 +20,27 @@
 <h2></h2>
 <section class="container">
 <article class="">
+<h3><b>Nuestro Trabajo</b></h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+<p><a href="#">Ver mas</a></p>
 
 </article>
 <article class="">
+  <h3><b>Equipo</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
 
 </article>
 <article class="">
+  <h3><b>Blog</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
 
 </article>
 <article class="">
+  <h3><b>Algo mas</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
 
 </article>
 </section>
