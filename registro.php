21898a03a73476cde1ed66c90e9688e6d54d4ef9
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-05-30 20:47:27 -0300

    Se agrega perfil.php, registro.php y servicios.php

---
+++ registro.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Proyecto</title>
+    	<link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+<header class="container">
+<nav>
+  <ul>
+    <li><a id="logo" href="nosotros">Construworld</a></li>
+    <li><a class="nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
+    <li><a class="nav1" href="servicios.php">Servicios</a></li>
+    <li><a class="nav1" href="registro.php">Registracion</a></li>
+    <li><a class="nav1" href="login.php">Login</a></li>
+  </ul>
+</nav>
+</header>
+<h2></h2>
+<section class="container">
+
+<form class="" action="index.html" method="post">
+  User <input type="text" name="user" value="">
+  Password <input type="password" name="password" value="">
+  <input type="submit" name="enviar" value="Enviar">
+
+</form>
+
+</section>
+<br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+  <a class="foot" href="contacto.php">Contacto</a>
+</footer>
+  </body>
+</html>
---
+++ servicios.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Proyecto</title>
+    	<link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+<header class="container">
+<nav>
+  <ul>
+    <li><a id="logo" href="nosotros">Construworld</a></li>
+    <li><a class="nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
+    <li><a class="nav1" href="servicios.php">Servicios</a></li>
+    <li><a class="nav1" href="registro.php">Registracion</a></li>
+    <li><a class="nav1" href="login.php">Login</a></li>
+  </ul>
+</nav>
+</header>
+<h2></h2>
+<section class="container">
+
+	<h1>Arquitectura.</h1><br>
+	<h1>Maestro Mayor de Obra (constructor).</h1><br>
+	<h1>Electricidad.</h1><br>
+	<h1>Plomería.</h1><br>
+	<h1>Gas.</h1><br>
+	<h1>Carpintería.</h1><br>
+
+</section>
+<br><br><br><br><br><br><br><br><br><br><br>
+<footer>
+  <a class="foot" href="contacto.php">Contacto</a>
+</footer>
+  </body>
+</html>
