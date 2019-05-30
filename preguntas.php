601d1416d90bede64c24943beed3eea8afb598dc
MinoLou <50384435+MinoLou@users.noreply.github.com>
2019-05-30 00:04:38 -0300

    Add files via upload
    Agregué preguntas frecuentes, sin css pq tengo sueño :)

---
+++ preguntas.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Preguntas frecuentes</title>
+  </head>
+  <body>
+    <h1>Preguntas frecuentes</h1>
+    <h2>¿Cómo selecciona Construworld sus proveedores?</h2>
+    <h3>Mediante una entrevista en profundidad se evalúa su capacidad técnica y operativa, honestidad y cordialidad. También se investigan referencias e historial laboral.
+    Asimismo con las calificaciones que otorgan los usuarios del servicio, se cuenta con una una evaluación permanente y dinámica de cada prestador acerca de su idoneidad, buen trato y precios justos.</h3>
+    <br>
+    <h2>¿Qué ventajas ofrece Construworld?</h2>
+    <h3><ol>
+      <li>Reune prestadores calificados, seleccionados de manera idónea;</li>
+      <li>Que se encuentran disponibles mediante un click, o a traves de nuestra central telefónica;</li>
+      <li>Atención de su consulta en 48hs o menos;</li>
+      <li>Servicios de urgencia;</li>
+      <li>Presupuesto sin cargo en domicilio;</li>
+      <li>Lista de precios orientativos por rubros;</li>
+    </ol>
+    </h3>
+    <br>
+    <h2>¿Quiénes pueden utilizar Construworld?</h2>
+    <h3>Cualquier persona mayor de 18 años, residente en CABA y área metropolitana (previa aceptación de los Términos de Uso de Servicios).</h3>
+    <br>
+    <h2>¿Construworld emite facturas por los servicios?</h2>
+    <h3> Los prestadores de Construworld son <strong>PyMEs o trabajadores independientes habilitados<em>, por los que ellos se encuentran obligados a emitir facturas por los trabajos realizados.</h3>
+    <br>
+    <h2>¿Puedo trabajar para Construworld?</h2>
+    <h3>Si vos o tu empresa quieren formar parte de Construworld, contactanos a ... y comenzaremos con el proceso de admisión.</h3>
+    <br>
+  </body>
+</html>
