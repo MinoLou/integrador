477704dec03437d92eec453dc6cc575494bb749a
MinoLou <50384435+MinoLou@users.noreply.github.com>
2019-06-01 17:51:23 -0300

    Add files via upload

--- header.php
+++ header.php
@@ -3,6 +3,15 @@
   <head>
     <meta charset="utf-8">
       <title>Proyecto</title>
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     	<link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
@@ -19,4 +28,4 @@
       </nav>
     </div>
   </header>
-  <!--<div class="container">-->
+  <div class="container">
