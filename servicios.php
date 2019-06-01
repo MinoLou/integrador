d1c5806912b22802c854c0b42c6338e4ceb8b79e
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-01 15:40:54 -0300

    Cambios en servicios.php y en css

--- servicios.php
+++ servicios.php
@@ -1,15 +1,58 @@
 <?php require('header.php'); ?>
 
 <h2></h2>
-<section class="container">
-
-	<h1>Arquitectura.</h1><br>
-	<h1>Maestro Mayor de Obra (constructor).</h1><br>
-	<h1>Electricidad.</h1><br>
-	<h1>Plomería.</h1><br>
-	<h1>Gas.</h1><br>
-	<h1>Carpintería.</h1><br>
-
+<section class="profile-container">
+	<div class="side-menu">
+		<div class="side-menu-button">Item 1</div>
+		<div class="side-menu-button">Item 2</div>
+		<div class="side-menu-button">Item 3</div>
+		<div class="side-menu-button">Item 4</div>
+		<div class="side-menu-button">Item 5</div>
+		<div class="side-menu-button">Item 6</div>
+	</div>
+	<div class="main-profile">
+		<div class="service-tab">
+			<h2 style="text-align: center;">Arquitectura</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>Arq. Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	
+		<div class="service-tab">
+			<h2 style="text-align: center;">Maestro Mayor de Obra</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>MMO. Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	
+		<div class="service-tab">
+			<h2 style="text-align: center;">Electricidad</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>Ing. Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	
+		<div class="service-tab">
+			<h2 style="text-align: center;">Plomería</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	
+		<div class="service-tab">
+			<h2 style="text-align: center;">Gas</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	
+		<div class="service-tab">
+			<h2 style="text-align: center;">Carpintería</h2>
+			<img src="images/bill_gates.jpg"  alt="Arquitecto" class="img-service-tab">
+			<p>Bill Gates: 10 calificaciones positivas</p>
+			<button class="service-tab-button">Ver usuario</button>
+		</div>
+	</div>
 </section>
 <br><br><br><br><br><br><br><br><br><br><br>
 
