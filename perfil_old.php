80b2d3dc2f8ceba1d00187772356aca8b7e656cd
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-02 00:08:03 -0300

    Colores css

---
+++ perfil_old.php
@@ -0,0 +1,12 @@
+<?php require('header.php'); ?>
+
+<h2></h2>
+<section class="container">
+
+	<h1>(FOTITO).</h1><br>
+	<h1>Bienvenido, Usuario.</h1><br>
+
+</section>
+<br><br><br><br><br><br><br><br><br><br><br>
+
+<?php require('footer.php'); ?>
