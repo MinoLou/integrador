1e6e70a3348ca78b6d6b071c847e8e8a760c1282
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-01 22:12:38 -0300

    login, registro, index - prueba y css

--- registro.php
+++ registro.php
@@ -1,14 +1,15 @@
 <?php require('header.php'); ?>
 
-<h2></h2>
 <section class="container">
+	<h2>Registrate como usuario</h2>
+</section>
 
-<form class="" action="index.html" method="post">
-  User <input type="text" name="user" value="">
-  Password <input type="password" name="password" value="">
-  <input type="submit" name="enviar" value="Enviar">
-
-</form>
+<section class="container">
+	<form class="" action="index.html" method="post">
+		User <input type="text" name="user" value="">
+		Password <input type="password" name="password" value="">
+		<input type="submit" name="enviar" value="Enviar">
+	</form>
 
 </section>
 <br><br><br><br><br><br><br><br><br><br><br>
