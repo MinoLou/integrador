91446ae0bcad72e47c6a311ff73cb8b0bc619c29
MinoLou <50384435+MinoLou@users.noreply.github.com>
2019-06-02 18:35:54 -0300

    Add files via upload

---
+++ serviceList.php
@@ -0,0 +1,47 @@
+<?php
+
+	$serviceList = [
+		[
+			'id' => 1,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Arquitecto',
+			'description' => 'Proyectos, Dirección de Obra, Renderizaciones',
+			'ranking' => 10,
+		],
+    [
+			'id' => 2,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Maestro Mayor de Obra',
+			'description' => 'Elaboro soluciones constructivas, técnicas/estéticas, espaciales y económicas para un programa de necesidades determinado. Evaluo y defino las soluciones técnico constructivas más apropiadas, gestionar la planificación y documentación del proceso constructivo. Dirijo, gestiono y administro la ejecución de procesos constructivos y comunico a terceros acontecimientos que surjan en el mismo.',
+			'ranking' => 10,
+		],
+		[
+			'id' => 3,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Electricidad',
+			'description' => 'Instalación y mantenimiento de servicios eléctricos, tales como la iluminación, la electricidad y la calefacción. Trabajo en una amplia gama de edificios, casas, oficinas, fábricas, hospitales y centrales eléctricas. Reparación de fallos en instalaciones domésticas y en empresas.',
+			'ranking' => 6,
+		],
+    [
+			'id' => 4,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Plomería',
+			'description' => 'Instalaciones de agua potable, agua no potable y la recogida de aguas pluviales y de aguas residuales en las viviendas, locales comerciales o industrias y talleres. Cocina, baño y calefacción: reparación de tuberías, desagües, roturas de bajantes, desatascos, grifería, filtraciones.',
+			'ranking' => 8,
+		],
+    [
+			'id' => 5,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Gas',
+			'description' => 'Instalaciones y Arreglos Generales de artefactos de gas.',
+			'ranking' => 6,
+		],
+    [
+			'id' => 6,
+			'image' => 'bill_gates.jpg',
+			'title' => 'Carpintería',
+			'description' => 'Construcción de estructuras y accesorios:ventanas, techos, pisos, paredes y puertas de madera y otras estructuras de madera. Muebles de interior, alacenas, bajomesadas, rack, etc.',
+			'ranking' => 9,
+		],
+  ];
+?>
---
+++ servicios2.php
@@ -0,0 +1,25 @@
+<?php require('header.php');
+	require_once 'serviceList.php';
+  ?>
+
+<!-- Servicios -->
+<div class="container" style="margin-top:30px; margin-bottom: 30px;">
+  <div class="row">
+    <!-- Los servicios -->
+    <?php foreach ($serviceList as $oneService) : ?>
+      <div class="col-md-6 col-lg-4">
+				<div class="card">
+					<img src="images/<?= $oneService['image'] ?>" class="card-img-top" alt="imagen del servicio">
+					<div class="card-body">
+						<h5 class="btn btn-success"><?= $oneService['title'] ?></h5>
+						<p class="card-text"><?= substr($oneService['description'], 0, 150) ?>...
+            </p>
+						<p class="card-text">
+							<?php for ($i = 1; $i <= $oneService['ranking']; $i++): ?>
+								<i class="fa fa-star"></i>
+							<?php endfor; ?>
+						</p>
+          </div>
+				</div>
+			</div>
+			<?php endforeach; ?>
