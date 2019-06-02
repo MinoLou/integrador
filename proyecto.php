0e278fcb8d53d9a5ffdab65cfe7915f096bdff05
juanmartinibarguren@gmail.com <juanmartinibarguren@gmail.com>
2019-06-02 01:22:29 -0300

    Modifique cuestiones del CSS relacionadas a Registro. Arme la pagina registro

---
+++ proyecto.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Proyecto</title>
+    	<link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+<header class="container">
+<nav>
+  <ul>
+    <li><a id="logo" href="nosotros">Construworld</a></li>
+    <li><a class="nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
+    <li><a class="nav1" href="servicios.php">Servicios</a></li>
+    <li><a class="nav1" href="registro.php">Registracion</a></li>
+    <li><a class="nav1" href="login.php">Login</a></li>
+  </ul>
+</nav>
+</header>
+<h2></h2>
+<section class="container">
+<article class="">  
+<h3><b>Nuestro Trabajo</b></h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+<p><a href="#">Ver mas</a></p>
+
+</article>
+<article class="">
+  <h3><b>Equipo</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
+
+</article>
+<article class="">
+  <h3><b>Blog</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
+
+</article>
+<article class="">
+  <h3><b>Algo mas</b></h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p>
+  <p><a href="#">Ver mas</a></p>
+
+</article>
+</section>
+<footer>
+  <a class="foot" href="contacto.php">Contacto</a>
+</footer>
+  </body>
+</html>
--- registro.php
+++ registro.php
@@ -1,16 +1,75 @@
-<?php require('header.php'); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Registro</title>
+  </head>
+  <body class="registro-body">
+    <?php require('header.php'); ?>
 
-<h2></h2>
-<section class="container">
+    <br>
+    <br>
 
-<form class="" action="index.html" method="post">
-  User <input type="text" name="user" value="">
-  Password <input type="password" name="password" value="">
-  <input type="submit" name="enviar" value="Enviar">
+    <p><h3 class="registro">Registrate acá para que todos puedan contar con tus servicios</h3></p>
+<br>
+<br>
+<br>
+<br>
+<br>
 
-</form>
+<div class="form">
+  <form class="" action="index.html" method="post">
+    <!--<p class="blanco">Nombre Completo:</p>-->
+      <p><input class="redondeadocolor" type="text" name="nombre" value="" placeholder="Nombre">
+      <input class="redondeadocolor" type="text" name="apellido" value="" placeholder="Apellido"></p>
+      <br>
+      <br>
+      <br>
+      <p class="blanco">Elegí tu profesión</p>
+      <select class="redondeadocolor" name="profesion">
+        <option value="arquitecto">Arquitect@</option>
+        <option value="plomero">Plomer@</option>
+        <option value="maestro mayor de obra">Maestr@ mayor de Obr@</option>
+        <option value="gasista">Gasist@</option>
+        <option value="electricista">Electricist@</option>
+      </select>
+      <br>
+      <br>
+      <br>
+      <p class="blanco">Sexo:</p>
+      <!--<input type="radio" name="genero" value="male" checked> Masculino<br>
+      <input type="radio" name="genero" value="female"> Femenino<br>
+      <input type="radio" name="genero" value="other"> Otro-->
 
-</section>
-<br><br><br><br><br><br><br><br><br><br><br>
+      <select class="redondeadocolor" name="sexo">
+        <option value="Masculino">Masculino</option>
+        <option value="Femenino">Femenino</option>
+        <option value="Otro">Otro</option>
+      </select>
+      <br>
+      <br>
+      <br>
+      <!--<p class="blanco">Elegí tu Ciudad:</p>-->
+      <p><input class="redondeadocolor" type="text" name="ciudad" value="" placeholder="Completá con tu Ciudad"></p>
+      <br>
+      <!--<p class="blanco">Fecha de Nacimiento:</p>-->
+      <p><input class="redondeadocolor" type="text" name="fecha-nacimiento" value="" placeholder="Fecha de Nacimiento"></p>
+      <br>
+      <!--<p class="blanco">Completá con tu E-mail:</p>-->
+      <p><input class="redondeadocolor" type="email" name="email" value="" placeholder="E-mail"></p>
+      <br>
+      <!--<p class="blanco">Elegí tu contraseña</p>-->
+      <p><input class="redondeadocolor" type="password" name="password" value=""placeholder="Elegí tu contraseña "></p>
+      <br>
+      <!--<p class="blanco">Confirmá la contraseña, por favor</p>-->
+      <p><input class="redondeadocolor" type="password" name="rePassword" value="" placeholder="Reconfirmá tu contraseña acá"></p>
+      <br>
+      <br>
+      <input class="boton-registro"type="submit" name="enviar" value="Registrarme">
+  </form>
+</div>
+    <?php require('footer.php'); ?>
 
-<?php require('footer.php'); ?>
+  </body>
+</html>
