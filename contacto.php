22b61ebf6ccebf7170096c546ffb22ce50fe7c27
MinoLou <50384435+MinoLou@users.noreply.github.com>
2019-06-03 19:10:10 -0300

    falla al ingresar el contacto

---
+++ contacto.php
@@ -0,0 +1,19 @@
+<?php
+<form method="post" action="formulario.php" style="margin-left: 40px;"><br>
+  <p>
+    <br><label for="nombre">Nombre: </label>
+    <input id="nombre" type="text" name="nombre">
+  </p>
+  <p>
+    <label for="">E - mail : </label>
+    <input id="email" type="text" name="email">
+  </p>
+  <p>
+    <label for="">Mensaje: </label>
+    <input id="mensaje" type="text" name="mensaje">
+  </p>
+  <p>
+    <input id="enviar" type="submit" value="Enviar">
+  </p>
+</form>
+ ?>
--- footer.php
+++ footer.php
@@ -1,28 +1,8 @@
-  <!--</div>-->
+<html>
   <footer class="page-footer">
-    <div style="background-image: url('images/patron-ladrillos.jpg');">
-      <a href="contacto.php" style="font-size: 20px;">Contacto</a><br>
-		<form method="post" action="formulario.php" style="margin-left: 40px;"><br>
-			<p>
-				<br><label for="nombre">Nombre: </label>
-				<input id="nombre" type="text" name="nombre">
-			</p>
-			<p>
-				<label for="">E - mail : </label>
-				<input id="email" type="text" name="email">
-			</p>
-			<p>
-				<label for="">Mensaje: </label>
-				<input id="mensaje" type="text" name="mensaje">
-			</p>
-			<p>
-				<input id="enviar" type="submit" value="Enviar">
-			</p>
-		</form>
-    </div>
-	<div style="background-color: black; height: 110px;">
-		<p class="footer-legend" style="color: white; padding: 20px;">Copyright 2019 - The Bill Gates Group @ Digital House Lima.<br>Buenos Aires, Argentina <br>Todos los derechos reservados.</p>
+		<div style="background-color: black; height: 12vh;">
+		<p class="footer-legend" style="color: white; padding: 20px;">Copyright 2019 - The Bill Gates Group @ Digital House Lima.<br>Buenos Aires, Argentina <br>Todos los derechos reservados.<br>
+    <a href="contacto.php">Contacto</a><br></p>
 	</div>
   </footer>
-  </body>
-</html>
+  </html>
