93a803b80a5c66719bd27adad7398c3dfdf2376b
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-02 22:43:34 -0300

    Pequeño cambio en el footer

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
   <!--</div>-->
   <footer class="page-footer">
     <div style="background-image: url('images/patron-ladrillos.jpg');">
-      <a href="contacto.php" style="">Contacto</a><br>
+      <a href="contacto.php" style="font-size: 20px;">Contacto</a><br>
 		<form method="post" action="formulario.php" style="margin-left: 40px;"><br>
 			<p>
-				<label for="nombre">Nombre: </label>
+				<br><label for="nombre">Nombre: </label>
 				<input id="nombre" type="text" name="nombre">
 			</p>
 			<p>
-				<label for="">E-mail: </label>
+				<label for="">E - mail : </label>
 				<input id="email" type="text" name="email">
 			</p>
 			<p>
 				<label for="">Mensaje: </label>
-				<input id="mensaje" type="" name="mensaje">
+				<input id="mensaje" type="text" name="mensaje">
 			</p>
 			<p>
 				<input id="enviar" type="submit" value="Enviar">
