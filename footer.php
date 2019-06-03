7084d541b48fd3b6adf4a401adf5e9b25dfc42a9
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-03 20:09:12 -0300

    Borro header y footer

--- footer.php
+++
@@ -1,9 +0,0 @@
-  </div>
-  <footer class="page-footer">
-    <div class="containerb">
-		  <p class="footer-legend">Copyright 2019 - The Bill Gates Group @ Digital House Lima.</p>
-      <p>Podes contactarnos <a href="contacto.php"> aqui </a></p>
-	   </div>
-   </footer>
-   </body>
-</html>
