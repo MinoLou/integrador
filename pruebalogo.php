2cf8d0dfc5f86d4839b24e1e6f25ebd46b8fef7a
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-06-02 23:11:21 -0300

    Pruebitas para un logo

---
+++ pruebalogo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Proyecto</title>
+      <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+  <header class="page-headerb">
+    <div class="page-header-containerb containerb">
+      <a id="logo" href="index.php"><img src="images/logocw_transp.png" alt="Logo"></a>
+      <nav class="main-nav">
+        <ul>
+          <li><a class="nav-link nav1" href="preguntas.php">Preguntas Frecuentes</a></li>
+          <li><a class="nav-link nav1" href="servicios.php">Servicios</a></li>
+          <li><a class="nav-link nav1" href="registro.php">Registracion</a></li>
+          <li><a class="nav-link nav1" href="login.php">Login</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  </body>
+</html>
+
+
+
