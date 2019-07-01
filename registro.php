fe85ca5d0d165ccce51d8d5eb88073463364b605
MinoLou <50384435+MinoLou@users.noreply.github.com>
2019-07-01 16:37:02 -0300

    select para la nacionalidad, ver img p/ registro?

--- registro.php
+++ registro.php
@@ -1,3 +1,13 @@
+<?php
+$nacionalidades = [
+"AR" => "Argentina",
+"UY" => "Uruguay",
+"BR" => "Brasil",
+"PY" => "Paraguay",
+"CH" => "Chile "
+];
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -19,11 +29,13 @@
 <br>
 
 <div class="form">
-  <form class="" action="registroaction.php" method="post" enctype="multipart/form-data">
+  <form class="" action="registroaction.php" method="post">
     <!--<p class="blanco">Nombre Completo:</p>-->
       <p><input class="redondeadocolor" type="text" name="nombre" value="" placeholder="Nombre">
       <input class="redondeadocolor" type="text" name="apellido" value="" placeholder="Apellido"></p>
       <br>
+    <!--<p class="blanco">Elegí tu Nombre de Usuario:</p>-->
+      <p><input class="redondeadocolor" type="text" name="nombreusu" value="" placeholder="Usuario">
       <br>
       <br>
       <p class="blanco">Elegí tu profesión</p>
@@ -34,7 +46,6 @@
         <option value="gasista">Gasist@</option>
         <option value="electricista">Electricist@</option>
       </select>
-      <br>
       <br>
       <br>
       <p class="blanco">Sexo:</p>
@@ -49,12 +60,20 @@
       </select>
       <br>
       <br>
-      <br>
-      <!--<p class="blanco">Elegí tu Nombre de Usuario:</p>-->
-      <p><input class="redondeadocolor" type="text" name="nombreusu" value="" placeholder="Nombre de Usuario"></p>
-      <br>
       <!--<p class="blanco">Elegí tu País:</p>-->
-      <p><input class="redondeadocolor" type="text" name="pais" value="" placeholder="Completá con tu País"></p>
+      <p class="blanco">Nacionalidad</p>
+      <p><select class="redondeadocolor" type="text" name="pais" value="" placeholder="Elegí un país"></p>
+          <?php foreach ($nacionalidades as $key => $nacionalidad) : ?>
+            <?php if($_GET['nacionalidades'] == $key) : ?>
+            <option value="<?=$key?>" selected><?=$nacionalidad?>
+            </option>
+          <?php else : ?>
+            <option value="<?=$key?>"><?=$nacionalidad?>
+            </option>
+          <?php endif; ?>
+          <?php endforeach; ?>
+        </select>
+      <br>
       <br>
       <!--<p class="blanco">Fecha de Nacimiento:</p>-->
       <!--<p><input class="redondeadocolor" type="text" name="fecha-nacimiento" value="" placeholder="Fecha de Nacimiento"></p>
@@ -69,8 +88,6 @@
       <p><input class="redondeadocolor" type="password" name="rePassword" value="" placeholder="Reconfirmá tu contraseña acá"></p>
       <br>
       <br>
-	  <label for="archivo">Imagen de perfil (jpg, bmp): </label>
-		<input type="file" name="archivo"><br>
       <input class="boton-registro"type="submit" name="enviar" value="Registrarme">
   </form>
   <br><br>
