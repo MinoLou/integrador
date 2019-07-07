71bf8ed56cad1386f33b1a349a40a4507761c708
noe-mai <noelia.s.maiquez@gmail.com>
2019-07-07 13:08:06 -0300

    mas de servicios

--- servicios2.php
+++ servicios2.php
@@ -6,19 +6,18 @@ require_once 'serviceList.php';
 <!-- Servicios -->
 <div class="containerb" >
 
-<!-- Los servicios -->
-<?php foreach ($serviceList as $oneService) : ?>
-  <div class="card-body">
-  	<p class= "btn btn-outline-dark"><?= $oneService['title'] ?></p>
-  	<p class="card-text"><?= substr($oneService['description'], 0, 150) ?>...</p>
-
-		</p>
-     <div class="construction-icons">
-       <i class="fa fa-<?= $oneService['icon'] ?>"></i>
-     </div>
-	</div>
-<?php endforeach; ?>
-
+<div class="servicios-list">
+  <!-- Los servicios -->
+  <?php foreach ($serviceList as $oneService) : ?>
+    <div class="card-body">
+    	<p class= ""><?= $oneService['title'] ?></p>
+      <div class="construction-icons">
+        <i class="fa fa-<?= $oneService['icon'] ?>"></i>
+      </div>
+    	<p class="card-text"><?= substr($oneService['description'], 0, 150) ?>...</p>
+  	</div>
+  <?php endforeach; ?>
+  </div>
 </div>
 
 <?php require('footer.php'); ?>
