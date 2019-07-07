d6e9b4c04f1ebfbaa09e1a939de7730326b6e68c
noe-mai <noelia.s.maiquez@gmail.com>
2019-07-06 21:04:28 -0300

    cambios en servicios

--- serviceList.php
+++ serviceList.php
@@ -6,6 +6,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Arquitecto',
 			'description' => 'Proyectos, Dirección de Obra, Renderizaciones',
+			'icon' => 'angle-double-down',
 			'ranking' => 10,
 		],
     [
@@ -13,6 +14,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Maestro Mayor de Obra',
 			'description' => 'Elaboro soluciones constructivas, técnicas/estéticas, espaciales y económicas para un programa de necesidades determinado. Evaluo y defino las soluciones técnico constructivas más apropiadas, gestionar la planificación y documentación del proceso constructivo. Dirijo, gestiono y administro la ejecución de procesos constructivos y comunico a terceros acontecimientos que surjan en el mismo.',
+			'icon' => '',
 			'ranking' => 10,
 		],
 		[
@@ -20,6 +22,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Electricidad',
 			'description' => 'Instalación y mantenimiento de servicios eléctricos, tales como la iluminación, la electricidad y la calefacción. Trabajo en una amplia gama de edificios, casas, oficinas, fábricas, hospitales y centrales eléctricas. Reparación de fallos en instalaciones domésticas y en empresas.',
+			'icon' =>'jedi',
 			'ranking' => 6,
 		],
     [
@@ -27,6 +30,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Plomería',
 			'description' => 'Instalaciones de agua potable, agua no potable y la recogida de aguas pluviales y de aguas residuales en las viviendas, locales comerciales o industrias y talleres. Cocina, baño y calefacción: reparación de tuberías, desagües, roturas de bajantes, desatascos, grifería, filtraciones.',
+			'icon' => '',
 			'ranking' => 8,
 		],
     [
@@ -34,6 +38,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Gas',
 			'description' => 'Instalaciones y Arreglos Generales de artefactos de gas.',
+			'icon' => '',
 			'ranking' => 6,
 		],
     [
@@ -41,6 +46,7 @@
 			'image' => 'bill_gates.jpg',
 			'title' => 'Carpintería',
 			'description' => 'Construcción de estructuras y accesorios:ventanas, techos, pisos, paredes y puertas de madera y otras estructuras de madera. Muebles de interior, alacenas, bajomesadas, rack, etc.',
+			'icon' => '',
 			'ranking' => 9,
 		],
   ];
--- servicios2.php
+++ servicios2.php
@@ -4,27 +4,23 @@ require_once 'serviceList.php';
   ?>
 
 <!-- Servicios -->
-<div class="container" style="margin-top:30px; margin-bottom: 30px;">
-  <div class="row" align-items-center>
-    <!-- Los servicios -->
-    <?php foreach ($serviceList as $oneService) : ?>
-      <div class="col-sm-12 col-md-6 col-lg-4">
-				<div class="card">
-					<img src="images/<?= $oneService['image'] ?>" class="card-img-top" alt="imagen del servicio">
-					<div class="card-body">
-						<h5 class="btn btn-success"><?= $oneService['title'] ?></h5>
-						<p class="card-text"><?= substr($oneService['description'], 0, 150) ?>...
-            </p>
-						<p class="card-text">
-							<?php for ($i = 1; $i <= $oneService['ranking']; $i++): ?>
-								<i class="fa fa-star"></i>
-							<?php endfor; ?>
-						</p>
-          </div>
-				</div>
-			</div>
-			<?php endforeach; ?>
+<div class="containerb" >
+
+<!-- Los servicios -->
+<?php foreach ($serviceList as $oneService) : ?>
+  <div class="card-body">
+  	<p class="btn btn-warning"><?= $oneService['title'] ?></p>
+  	<p class="card-text"><?= substr($oneService['description'], 0, 150) ?>...</p>
+			<?php for ($i = 1; $i <= $oneService['ranking']; $i++): ?>
+				<i class="fa fa-jedi"></i>
+			<?php endfor; ?>
+		</p>
+     <div class="construction-icons">
+     <i class="fa fa-<?= $oneService['icon'] ?>"></i>
+     </div>
 	</div>
+<?php endforeach; ?>
+
 </div>
 
 <?php require('footer.php'); ?>
