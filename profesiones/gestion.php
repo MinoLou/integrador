8d806a23642d62dcc3aeb2894b9c1c67133bdb72
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-07-09 22:52:45 -0300

    Avances en archivos por profesion en nuevo directorio

--- profesiones/gestion.php
+++ profesiones/gestion.php
@@ -12,6 +12,21 @@ require_once '../serviceList.php';
   }?>
 <html>
 	<p class="registro">Gestión de usuario <?= $_SESSION['nombre']?> con <?=$_GET['profesion']?> <?=$_GET['nombre']?></p>
+	
+	<div class="contenedor-imagenes-perfil">
+		<img height="50px" src="../images/bill_gates.jpg" alt="Usuario" class="profile-picture">
+		<img height="50px" src="../images/bill_gates.jpg" alt="Usuario" class="profile-picture">
+	</div>
+	
+	<form method="post" action="formulario.php" style="margin-left: 30px;">
+    <label for="">Enviar mensaje a prestador: </label></br>
+    <input id="mensaje" type="text" name="mensaje" maxlength="100" style="height: 100px;">
+    </p>
+    <p>
+    <input id="enviar" type="submit" value="Enviar">
+    </p>
+  </form>
+	
 </html>
 
 <?php
