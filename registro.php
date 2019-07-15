4bcd636345fb18f894b8c47d761abdffeb63411f
LeandroBinetti87 <pinosoftcom@gmail.com>
2019-07-14 23:36:24 -0300

    Estilos de Noe para regusu.php y registro.php

--- registro.php
+++ registro.php
@@ -15,21 +15,15 @@ $nacionalidades = [
     <link rel="stylesheet" href="css/styles.css">
     <title>Registro</title>
   </head>
-  <body class="registro-body">
+  <body>
     <?php require('header.php'); ?>
 
-
-    <p><h3 class="registro">Registrate acá para que todos puedan contar con tus servicios</h3></p>
-
-
 <div class= "containerb">
+  <h1>Registrate acá para que todos puedan contar con tus servicios</h1>
   <form class="" action="registroaction.php" method="post" enctype="multipart/form-data">
     <!--<p class="blanco">Nombre Completo:</p>-->
       <p><input class="form-control" type="text" name="nombre" value="" placeholder="Nombre">
       <input class="form-control"type="text" name="apellido" value="" placeholder="Apellido"></p>
-      <br>
-      <br>
-      <br>
       <p class="blanco">Elegí tu profesión</p>
       <select class="form-control" name="profesion">
         <option value="arquitecto">Arquitect@</option>
@@ -39,25 +33,17 @@ $nacionalidades = [
         <option value="electricista">Electricist@</option>
         <option value="carpintero">Carpinter@</option>
       </select>
-      <br>
-      <br>
-      <br>
       <p class="blanco">Sexo:</p>
       <!--<input type="radio" name="genero" value="male" checked> Masculino<br>
       <input type="radio" name="genero" value="female"> Femenino<br>
       <input type="radio" name="genero" value="other"> Otro-->
-
       <select class="form-control" name="sexo">
         <option value="Masculino">Masculino</option>
         <option value="Femenino">Femenino</option>
         <option value="Otro">Otro</option>
       </select>
-      <br>
-      <br>
-      <br>
       <!--<p class="blanco">Elegí tu Nombre de Usuario:</p>-->
       <p><input class="form-control" type="text" name="nombreusu" value="" placeholder="Nombre de Usuario"></p>
-      <br>
        <!--<p class="blanco">Elegí tu País:</p>-->
       <p class="blanco">Nacionalidad</p>
       <p><select class="form-control"type="text" name="pais" value="" placeholder="Elegí un país"></p>
@@ -71,25 +57,18 @@ $nacionalidades = [
           <?php endif; ?>
           <?php endforeach; ?>
         </select>
-      <br>
-      <br>
       <!--<p class="blanco">Fecha de Nacimiento:</p>-->
       <!--<p><input class="redondeadocolor" type="text" name="fecha-nacimiento" value="" placeholder="Fecha de Nacimiento"></p>
       <br>-->
       <!--<p class="blanco">Completá con tu E-mail:</p>-->
       <p><input class="form-control"type="email" name="email" value="" placeholder="E-mail"></p>
-      <br>
       <!--<p class="blanco">Elegí tu contraseña</p>-->
       <p><input class="form-control" type="password" name="password" value=""placeholder="Elegí tu contraseña "></p>
-      <br>
       <!--<p class="blanco">Confirmá la contraseña, por favor</p>-->
       <p><input class= "form-control"type="password" name="rePassword" value="" placeholder="Reconfirmá tu contraseña acá"></p>
-      <br>
-      <br>
 	  <label for="archivo">Imagen de perfil (jpg, bmp): </label>
 		<input type="file" name="archivo"><br>
       <input class="boton-registro"type="submit" name="enviar" value="Registrarme">
   </form>
-  <br><br>
 </div>
     <?php require('footer.php'); ?>
