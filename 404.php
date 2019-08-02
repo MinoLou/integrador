92c69050c95981e1bccc9fa1bfab060c44c4c23f
LeandroBinetti <leandro7sfer@gmail.com>
2019-08-02 10:34:03 -0300

    para que devuelva 404 not found con nuestro estilo cuando le mandas una URL no valida

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php require('header.php'); ?>
+
+<!--<div class="containerb">-->
+
+<!-- Slideshow container -->
+
+<br><br><br>
+
+<div class="containerb">
+  <section class="intro-text">
+    <h1>Error 404</h1>
+    <p>Sitio no encontrado. Haz el favor de verificar la URL.</p>
+  </section>
+</div>
+
+<br><br><br><br>
+
+<?php require('footer.php'); ?>
